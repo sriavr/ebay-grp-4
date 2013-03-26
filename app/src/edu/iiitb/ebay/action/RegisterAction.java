29f696ce6b15aebdbe3a51aec8259f1103bc004d
ranadheerork@gmail.com <ranadheerork@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-03-26 15:01:38 +0000

    Added for Registration Page - Ranadheer

---
+++ app/src/edu/iiitb/ebay/action/RegisterAction.java
@@ -0,0 +1,11 @@
+package edu.iiitb.ebay.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+	
+	public String execute() {
+		
+		return SUCCESS;
+	}
+}
