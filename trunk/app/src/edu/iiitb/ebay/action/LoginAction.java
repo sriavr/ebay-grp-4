e85f303b261dc1c4569faf99cbb0092b58eeb443
debargha.ganguly@gmail.com <debargha.ganguly@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-01 21:07:14 +0000

    Login action

---
+++ trunk/app/src/edu/iiitb/ebay/action/LoginAction.java
@@ -0,0 +1,73 @@
+package edu.iiitb.ebay.action;
+import java.util.Map;
+
+	import com.opensymphony.xwork2.ActionContext;
+	import com.opensymphony.xwork2.ActionSupport;
+	import edu.iiitb.ebay.dao.AdminDAO;
+	import edu.iiitb.ebay.dao.UserDAO;
+	import edu.iiitb.ebay.model.entity.AdminModel;
+	import edu.iiitb.ebay.model.entity.UserModel;
+
+
+public class LoginAction {
+
+		private String userName="";
+		private String password="";
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * This method handles the login of administrator and student
+		 */
+
+		public String execute() {
+			Map<String, Object> sessionMap = ActionContext.getContext()
+					.getSession();
+			
+			if(userName.equals(""))
+				return "initial";
+			
+			AdminDAO adminDAO = new AdminDAO();
+			UserDAO userDao = new UserDAO();
+
+			AdminModel admin = adminDAO.login(userName, password);
+			if (admin != null) {
+				sessionMap.put("admin", admin);
+				sessionMap.put("role", "admin");
+				return "success";
+			} else {
+				UserModel user = userDao.login(userName, password);
+				
+				if (user == null) {
+					//addActionError("Wrong username/password combination");
+					return "error";
+				} else {
+					sessionMap.put("user", user);
+					sessionMap.put("role", "user");
+					return "success";
+				}
+			}
+		}
+		
+		
+
+	}
+
+
+
