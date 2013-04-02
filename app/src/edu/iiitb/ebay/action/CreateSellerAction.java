bc65c2afda5a1f2a8b141dddfa51bb8caa7638c9
debargha.ganguly@gmail.com <debargha.ganguly@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-02 19:47:29 +0000

    Create seller action--Debargha Ganguly

---
+++ app/src/edu/iiitb/ebay/action/CreateSellerAction.java
@@ -0,0 +1,64 @@
+package edu.iiitb.ebay.action;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import edu.iiitb.ebay.dao.CreateSellerAccountDAO;
+import edu.iiitb.ebay.model.entity.SellerModel;
+import edu.iiitb.ebay.model.entity.UserModel;
+
+public class CreateSellerAction extends ActionSupport {
+	
+	
+	String location;
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getRegister() {
+		return register;
+	}
+	public void setRegister(String register) {
+		this.register = register;
+	}
+	String register="";
+	public String execute()
+	{
+		Map<String, Object> sessionMap = ActionContext.getContext()
+		.getSession();
+		
+		if(sessionMap.get("user")==null)
+          return "initial";	
+		UserModel um = (UserModel) sessionMap.get("user");
+		CreateSellerAccountDAO dao = new CreateSellerAccountDAO();
+		if(!register.equals(""))
+		{
+			SellerModel sm = new SellerModel();
+			sm.setUserId(um.getUserId());
+			sm.setLocation(location);
+			dao.insertSeller(sm);
+		}
+		
+		SellerModel sm=dao.getSellerInfo(um.getUserId());
+		if(sm==null)
+		{	
+			return "register";
+		}
+		else
+		{
+		  sessionMap.put("seller", sm);
+		  return SUCCESS;
+		}
+		
+		
+		
+		
+	}
+		
+
+}
