69f96035bacba86b71c8dd7de4dbc40ea0814db2
ranadheerork@gmail.com <ranadheerork@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-07 22:29:51 +0000

    update price,discount,quantity of a product by its seller

---
+++ trunk/app/src/edu/iiitb/ebay/action/ViewMyProductsAction.java
@@ -0,0 +1,54 @@
+package edu.iiitb.ebay.action;
+
+import java.util.ArrayList;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import edu.iiitb.ebay.dao.BrowseDAO;
+import edu.iiitb.ebay.dao.SellerProductUpdationDAO;
+import edu.iiitb.ebay.model.entity.ProductModel;
+import edu.iiitb.ebay.model.entity.UserModel;
+
+// sellar viewing his own products. 
+public class ViewMyProductsAction extends ActionSupport{
+
+	int SellerId;
+	private ArrayList<ProductModel> products = new ArrayList<ProductModel>();
+	
+	public String execute(){
+		
+		//get the user id of the user 
+		int userId = ((UserModel)ActionContext.getContext().getSession().get("user")).getUserId();
+		System.out.println("user id : " + userId);
+		
+		//check whether that user id exists in sellar table or not ...if not he is not a sellar
+		SellerProductUpdationDAO sellerDAO = new SellerProductUpdationDAO();
+		
+		SellerId = sellerDAO.getSellerId(userId);
+		System.out.println("seller id : " + SellerId);
+		if(SellerId == -1){
+			//user is not registered seller ....throw some error as "ur not seller"
+			return NONE;
+		}
+		else{
+			//he is registered user - get the list of his own products 
+			setProducts(sellerDAO.getProducts(SellerId));
+		}
+		return SUCCESS;
+	}
+	
+	public int getSellerId() {
+		return SellerId;
+	}
+	public void setSellerId(int sellerId) {
+		SellerId = sellerId;
+	}
+	public ArrayList<ProductModel> getProducts() {
+		return products;
+	}
+	public void setProducts(ArrayList<ProductModel> products) {
+		this.products = products;
+	}
+	
+}
