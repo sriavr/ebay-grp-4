32f864337b01adea887ba5e7aaf492e36e150220
sriavr@gmail.com <sriavr@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-07 09:15:37 +0000

    changes for category data

--- SQL/ebay-updated-data_v2.sql
+++ SQL/ebay-updated-data_v2.sql
@@ -70,3 +70,20 @@ insert into deals (dealsId, productId, dealStartDate, dealEndDate, dealSellingPr
     (3007,207,'2013-03-03','2013-03-03',300),
     (3008,208,'2013-04-01','2013-05-03',200),
     (3009,209,'2013-04-10','2013-05-03',30);
+    
+delete from category;
+INSERT INTO `category` (`categoryId`, `categoryName`, `parentCategoryId`) VALUES
+	(1, 'Mobiles', 0),
+	(2, 'Accessories', 0),
+	(3, 'Computers', 0),
+	(4, 'Laptops', 3),
+	(5, 'PDA', 3),
+	(6, 'Audio Accessories', 2),
+	(7, 'Clothing', 2),
+	(8, 'Footwears', 2),
+	(9, 'Books and Magazines', 0),
+	(10, 'Movies and Music', 0),
+	(11, 'Sports Equipments', 0),
+	(12, 'Novels', 9),
+	(13, 'Home and Kitchen', 0),
+	(14, 'Fiction Books', 9);    
