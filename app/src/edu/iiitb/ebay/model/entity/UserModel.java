8a4522a20c719d6ab36cbb6d9a82df821d86c25d
pratibind.jha <pratibind.jha@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-08 20:19:56 +0000

    countryName and stateName data member has been added.

--- app/src/edu/iiitb/ebay/model/entity/UserModel.java
+++ app/src/edu/iiitb/ebay/model/entity/UserModel.java
@@ -1,10 +1,5 @@
 package edu.iiitb.ebay.model.entity;
 
-import java.io.File;
-import java.util.Date;
-import java.util.Timer;
-
-import org.apache.catalina.User;
 
 /**
  * 
@@ -14,74 +9,127 @@ import org.apache.catalina.User;
  */
 public class UserModel {
 	private Integer userId;
-	
+
 	private String firstName;
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getHomeAddress() {
 		return homeAddress;
 	}
+
 	public void setHomeAddress(String homeAddress) {
 		this.homeAddress = homeAddress;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public Integer getCountryId() {
 		return countryId;
 	}
+
 	public void setCountryId(Integer countryId) {
 		this.countryId = countryId;
 	}
+
 	public Integer getStateId() {
 		return stateId;
 	}
+
 	public void setStateId(Integer stateId) {
 		this.stateId = stateId;
 	}
+
 	public long getPinCode() {
 		return pinCode;
 	}
+
 	public void setPinCode(long pinCode) {
 		this.pinCode = pinCode;
 	}
+
 	public long getTelephoneNo() {
 		return telephoneNo;
 	}
+
 	public void setTelephoneNo(long telephoneNo) {
 		this.telephoneNo = telephoneNo;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	/**
+	 * @return the countryName
+	 */
+	public String getCountryName() {
+		return countryName;
+	}
+
+	/**
+	 * @param countryName
+	 *            the countryName to set
+	 */
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	/**
+	 * @return the stateName
+	 */
+	public String getStateName() {
+		return stateName;
+	}
+
+	/**
+	 * @param stateName
+	 *            the stateName to set
+	 */
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
 	private String lastName;
 	private String homeAddress;
 	private String city;
@@ -91,5 +139,7 @@ public class UserModel {
 	private long telephoneNo;
 	private String email;
 	private String password;
-	
+	private String countryName;
+	private String stateName;
+
 }
