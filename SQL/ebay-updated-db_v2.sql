ca6ec4404ffe564fd356dd41ec0fbae9fe94e262
yeleswarapu.pavan@gmail.com <yeleswarapu.pavan@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-08 15:29:06 +0000

    changes for deals

--- SQL/ebay-updated-db_v2.sql
+++ SQL/ebay-updated-db_v2.sql
@@ -570,9 +570,4 @@ BEGIN
     -- select queryString from dual;
     drop temporary table tmpcat;
 END$$
-
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DELIMITER ;
