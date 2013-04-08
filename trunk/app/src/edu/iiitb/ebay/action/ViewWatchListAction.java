2aeb23d1945e1fac222341d6e316c260a538169b
pratibind.jha <pratibind.jha@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-08 05:35:57 +0000

    Initial copy has added.

---
+++ trunk/app/src/edu/iiitb/ebay/action/ViewWatchListAction.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package edu.iiitb.ebay.action;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import edu.iiitb.ebay.dao.ViewWatchListDAO;
+import edu.iiitb.ebay.model.entity.UserModel;
+import edu.iiitb.ebay.model.page.ViewWatchListModel;
+
+/**
+ * @author Pratibind Kumar Jha
+ * 
+ */
+public class ViewWatchListAction extends ActionSupport {
+
+	private int watchListId;
+	private int productId;
+	private ArrayList<ViewWatchListModel> viewWatchListModel;
+
+	/**
+	 * @return the watchListId
+	 */
+	public int getWatchListId() {
+		return watchListId;
+	}
+
+	/**
+	 * @param watchListId
+	 *            the watchListId to set
+	 */
+	public void setWatchListId(int watchListId) {
+		this.watchListId = watchListId;
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public int getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId
+	 *            the productId to set
+	 */
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the viewWatchListModel
+	 */
+	public ArrayList<ViewWatchListModel> getViewWatchListModel() {
+		return viewWatchListModel;
+	}
+
+	/**
+	 * @param viewWatchListModel
+	 *            the viewWatchListModel to set
+	 */
+	public void setViewWatchListModel(
+			ArrayList<ViewWatchListModel> viewWatchListModel) {
+		this.viewWatchListModel = viewWatchListModel;
+	}
+
+	// Method for get List of watchlist.
+	public String viewWatchList() {
+		Map<String, Object> session;
+		int userId = 0;
+		UserModel userModel;
+		/*
+		 * session = ActionContext.getContext().getSession(); String Role = "";
+		 * Role = (String) session.get("role"); if (Role.equals("user")) {
+		 * userModel = (UserModel) session.get("user"); userId =
+		 * userModel.getUserId(); } else { addActionMessage("Please Login");
+		 * return "fail"; }
+		 */
+
+		// hardcoded data
+		userId = 15;
+		this.viewWatchListModel = ViewWatchListDAO.getWatchListModel(userId);
+		if (this.viewWatchListModel == null)
+			addActionMessage("There is no item in WatchList");
+		return "success";
+
+	}
+}
