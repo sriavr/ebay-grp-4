97f1ba65cdd2fc0bf0a1a8e47cb5276879f659bf
pratibind.jha <pratibind.jha@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-09 17:56:20 +0000

    syc with session!!!

--- app/src/edu/iiitb/ebay/dao/ViewWatchListDAO.java
+++ app/src/edu/iiitb/ebay/dao/ViewWatchListDAO.java
@@ -75,27 +75,55 @@ public class ViewWatchListDAO {
 	public static int addToWatchListDAO(int productId, int userId) {
 		int status = 0;
 		int watchListId = 0;
+		int flag = 0;
 		String Query = "SELECT max(userWishListId)as watchListId FROM userwishlist";
-		ResultSet rs;
+		// to check whether product has been all ready add by perticular user.
+		String Query2 = "SELECT userWishListId FROM userwishlist WHERE productId="
+				+ productId + " AND userId=" + userId;
+		ResultSet rs = null;
+		// check user has allready add the product into watchlist.
 		try {
-			rs = BaseDAO.readFromDB(Query);
+			rs = BaseDAO.readFromDB(Query2);
 			while (rs.next()) {
-				watchListId = rs.getInt("watchListId");
+				flag = 1;
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
+		} finally {
+			BaseDAO.close(rs);
 		}
-		String Query1 = "INSERT INTO userwishlist ( `userWishListId`, `userId`,`productId`) VALUES(?,?)";
+
+		if (flag == 1) {
+			return -1;
+		}
+
+		// retrive the next watchlistid.
+		try {
+			rs = BaseDAO.readFromDB(Query);
+			while (rs.next()) {
+				watchListId = rs.getInt("watchListId");
+				System.out.println("Max wishlist IS is : " + watchListId);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			BaseDAO.close(rs);
+		}
+		String Query1 = "INSERT INTO userwishlist ( `userWishListId`, `productId`,`userId`) VALUES(?,?,?)";
 		try {
 			DatabaseUtil.connect();
 			DatabaseUtil.ps = DatabaseUtil.con.prepareStatement(Query1);
 			DatabaseUtil.ps.setInt(1, watchListId + 1);
 			DatabaseUtil.ps.setInt(2, productId);
-			DatabaseUtil.ps.setInt(2, userId);
+			DatabaseUtil.ps.setInt(3, userId);
+			System.out.println("Prespare Statement: " + DatabaseUtil.ps);
 			status = DatabaseUtil.ps.executeUpdate();
+			System.out.println("Add To WATCHLIST: Query output is:  " + status);
 		} catch (Exception e) {
 			// TODO: handle exception
+			e.printStackTrace();
 		} finally {
 			DatabaseUtil.connectionClose();
 		}
