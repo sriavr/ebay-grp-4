e63c907aff173afc7d42f8f84b6f4940cac7c25c
pratibind.jha <pratibind.jha@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-12 14:28:19 +0000

    UserManagement usecase has been completed.

--- app/src/edu/iiitb/ebay/dao/MangeUserInfoDAO.java
+++ app/src/edu/iiitb/ebay/dao/MangeUserInfoDAO.java
@@ -3,6 +3,7 @@
  */
 package edu.iiitb.ebay.dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
@@ -18,8 +19,7 @@ public class MangeUserInfoDAO {
 	public static ArrayList<UserModel> getUserModelList(String whereClouse) {
 
 		ArrayList<UserModel> userModel = new ArrayList<UserModel>();
-		String Query = "SELECT * FROM user JOIN country ON user.countryId=country.countryId JOIN state "
-				+ " ON state.stateId=user.stateId " + whereClouse;
+		String Query = "SELECT * FROM user " + whereClouse;
 		ResultSet rs = null;
 		try {
 			rs = BaseDAO.readFromDB(Query);
@@ -44,8 +44,8 @@ public class MangeUserInfoDAO {
 			userModelObj.setLastName(rs.getString("lastName"));
 			userModelObj.setHomeAddress(rs.getString("homeAddress"));
 			userModelObj.setCity(rs.getString("city"));
-			userModelObj.setCountryName(rs.getString("country"));
-			userModelObj.setStateName(rs.getString("state"));
+			// userModelObj.setCountryName(rs.getString("country"));
+			// userModelObj.setStateName(rs.getString("state"));
 			userModelObj.setPinCode(rs.getInt("pinCode"));
 			userModelObj.setTelephoneNo(rs.getString("telephoneNo"));
 			userModelObj.setEmail(rs.getString("email"));
@@ -57,5 +57,32 @@ public class MangeUserInfoDAO {
 			e.printStackTrace();
 		}
 		return userModelObj;
+	}
+
+	// save the edited user info into database
+	public static int saveUserInfo(UserModel userModel) {
+		int status = 0;
+		try {
+			PreparedStatement ps = BaseDAO
+					.getConnection()
+					.prepareStatement(
+							"UPDATE user set firstName=?, "
+									+ " lastName=?, homeAddress=?, city=?, pinCode=?, telephoneNo=?, email=?, password=?  WHERE userId=?");
+			ps.setString(1, userModel.getFirstName());
+			ps.setString(2, userModel.getLastName());
+			ps.setString(3, userModel.getHomeAddress());
+			ps.setString(4, userModel.getCity());
+			ps.setLong(5, userModel.getPinCode());
+			ps.setString(6, userModel.getTelephoneNo());
+			ps.setString(7, userModel.getEmail());
+			ps.setString(8, userModel.getPassword());
+			ps.setInt(9, userModel.getUserId());
+			System.out.println("query:  " + ps.toString());
+			status = BaseDAO.update(ps);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return status;
 	}
 }
