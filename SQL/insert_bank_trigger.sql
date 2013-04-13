144f42f8647ef54d7203f1b4b6113bd037bcb374
pratibind.jha <pratibind.jha@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-13 20:44:43 +0000

    Initial has added to repo.

---
+++ SQL/insert_bank_trigger.sql
@@ -0,0 +1,28 @@
+-- --------------------------------
+---trigger to insert into bank table when uesr do the redistration.
+-- VERSION 1.0 last edited by pratibind Jha
+-- 		   
+-- --------------------------------
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+DROP TRIGGER IF EXISTS `insert_into_bank`; 
+
+DELIMITER $$
+USE `ebay`$$
+
+CREATE TRIGGER `insert_into_bank` AFTER INSERT ON `user`
+ FOR EACH ROW 
+ BEGIN 
+	 DECLARE t_bankId INT DEFAULT 0;
+	 DECLARE t_userId INT DEFAULT 0;
+	 DECLARE t_accountNum INT DEFAULT 0;
+	 DECLARE t_atmNum INT DEFAULT 0;
+	 SELECT MAX(userId) INTO t_userId FROM `user` ;
+	 SELECT MAX(bankId) INTO t_bankId  FROM bank;
+	 INSERT INTO bank (`userId`,`accountNum`,`atmNum`,`pinNum`,`password`,`balance`) VALUES
+	 	(t_userId,CONCAT("1234_",t_userId+1),'1234',4321,'9876',10000);
+ END$$
+DELIMITER ;
+
