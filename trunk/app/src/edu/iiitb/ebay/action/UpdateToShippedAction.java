40905842586568b0274e3e1efc13632135996c06
ranadheerork@gmail.com <ranadheerork@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-16 00:07:03 +0000

    added mailing code on updation of shipped

--- trunk/app/src/edu/iiitb/ebay/action/UpdateToShippedAction.java
+++ trunk/app/src/edu/iiitb/ebay/action/UpdateToShippedAction.java
@@ -1,8 +1,15 @@
 package edu.iiitb.ebay.action;
 
+import java.util.Date;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 import edu.iiitb.ebay.dao.SellerSoldProductsDAO;
+import edu.iiitb.ebay.dao.UserDAO;
 import edu.iiitb.ebay.model.entity.OrderModel;
 import edu.iiitb.ebay.model.entity.ProductModel;
 import edu.iiitb.ebay.model.entity.UserModel;
@@ -15,9 +22,33 @@ public class UpdateToShippedAction extends ActionSupport{
 		System.out.println("order to be updated : " + getOrderId());
 		
 		SellerSoldProductsDAO sellerDAO  = new SellerSoldProductsDAO();
+		UserDAO userDAO = new UserDAO();
+		
+		//get current date time stamp
+		Date now = new Date();
+		//System.out.println("date : " + now.toString());
+		
+		//get customer email addr
+		String custEmailId = sellerDAO.getEmailId(getOrderId());
+		//System.out.println("email id : " +custEmailId);
+		
+		String subject = "your ebay order id : " + getOrderId() + " is shipped";
+		String message = "your order " + getOrderId() + " is shipped on " + now.toString();
 		
 		//update the status of the order to shipped.
 		sellerDAO.updateToShipped(getOrderId());
+		
+		try {
+			userDAO.sendEmail(custEmailId, subject, message);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//send the order updated status mail to customer  
 		return SUCCESS;
 	}
 
