fa46c5a097a9287e4ca94861837602b249598d42
ranadheerork@gmail.com <ranadheerork@gmail.com@510139ee-4fd9-bda1-c61c-8cbe6cc5f072>
2013-04-16 00:09:31 +0000

    added sendMail() to userDAO

--- trunk/app/src/edu/iiitb/ebay/dao/UserDAO.java
+++ trunk/app/src/edu/iiitb/ebay/dao/UserDAO.java
@@ -8,6 +8,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.apache.log4j.Logger;
 
@@ -128,5 +143,54 @@ public class UserDAO extends BaseDAO {
 		// ps.setDate(12, );
 		update(ps);
 
+	}
+	
+	//send mail to user
+	public void sendEmail(String toAddress,String subject, String message) 
+			throws AddressException, MessagingException{
+				System.out.println("in mail");
+				String host = "smtp.gmail.com";
+				String port = "587";
+				final String userName = "ebay4.iiitb@gmail.com";
+				final String password = "ebay@ooad";
+				
+				// sets SMTP server properties
+				Properties properties = new Properties();
+				properties.put("mail.smtp.host", host);
+				properties.put("mail.smtp.port", port);
+				properties.put("mail.smtp.auth", "true");
+				properties.put("mail.smtp.starttls.enable", "true");
+				properties.put("mail.user", userName);
+				properties.put("mail.password", password);
+
+				// creates a new session with an authenticator
+				Authenticator auth = new Authenticator() {
+					public PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(userName, password);
+					}
+				};
+				Session session = Session.getInstance(properties, auth);
+
+				// creates a new e-mail message
+				Message msg = new MimeMessage(session);
+
+				msg.setFrom(new InternetAddress(userName));
+				InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+				msg.setRecipients(Message.RecipientType.TO, toAddresses);
+				msg.setSubject(subject);
+				msg.setSentDate(new Date());
+
+				// creates message part
+				MimeBodyPart messageBodyPart = new MimeBodyPart();
+				messageBodyPart.setContent(message, "text/html");
+
+				// creates multi-part
+				Multipart multipart = new MimeMultipart();
+				multipart.addBodyPart(messageBodyPart);
+
+				// sets the multi-part as e-mail's content
+				msg.setContent(multipart);
+				// sends the e-mail
+				Transport.send(msg);
 	}
 }
